e018a7a850f2e54baa6e3bba14b5fba5358c2fcc
eungobs <eungobs@gmail.com>
2024-03-27 17:41:53 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 """
-create a web cache
+Module to define the get_page function
 """
 import redis
 import requests
 
+# Connect to Redis
 rc = redis.Redis()
 
 def get_page(url: str) -> str:
-    """ get a page and cache value """
+    """Fetch HTML content from a URL and cache it"""
+    # Increment the count for this URL
     rc.incr(f"count:{url}")
+
+    # Get the current count
     count = rc.get(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, requests.get(url).text)
-    return rc.get(f"cached:{url}").decode('utf-8')
+
+    # Fetch HTML content from the URL
+    html_content = requests.get(url).text
+
+    # Cache the HTML content with an expiration time of 10 seconds
+    rc.setex(f"cached:{url}", 10, html_content)
+
+    return html_content
 
 if __name__ == "__main__":
+    # Test the get_page function
     print(get_page('http://slowwly.robertomurray.co.uk'))
